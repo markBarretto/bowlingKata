5baf955fc2b31ebbc68efb92a11779f9723ffe2b
Mark Barretto <mark.barretto@gmail.com>
2014-11-28 17:27:35 +0800

    Added verification that the game exhists

---
+++ jasmine/spec/BowlingSpec.js
@@ -0,0 +1,11 @@
+//rules taken from http://slocums.homestead.com/gamescore.html
+
+describe("Bowling Kata", function(){
+	it("bowlingGame should exist", function(){
+		expect(bowlingGame).toBeTruthy(); //not null, undefined...
+	});
+	
+	it("bowlingGame should exist", function(){
+		expect(bowlingGame).toBeTruthy(); //not null, undefined...
+	});
+})
